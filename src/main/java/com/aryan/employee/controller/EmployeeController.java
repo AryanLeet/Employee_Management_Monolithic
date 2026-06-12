489d90274ae42e2f45a191d7cee0b7b4b12a491b
AryanLeet <apanjiyar70@gmail.com>
2026-06-13 02:16:19 +0530

    Create Employee controller

---
+++ src/main/java/com/aryan/employee/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.aryan.employee.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.aryan.employee.entity.Employee;
+import com.aryan.employee.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
